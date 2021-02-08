b21ea6815172ad730f3e432759116654ceb44001
Satchel Grant <grantsrb@stanford.edu>
2021-02-07 16:37:29 -0800

    add contrast models to imports

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,6 @@ from .conv_model import ConvModel
 from .fc_model import FCModel
 from .a3c_model import A3CModel
 from .inv_dynamics import InvDynamics
-from .embedder import Embedder, ConvEmbedder, CatModule, Ensemble
+from .embedder import Embedder, ConvEmbedder, CatModule, Ensemble, SimpleContrast, ContrastAttnModel
 from .gru_model import GRUModel
 from .rnnlocator import RNNLocator, SimpleDeconv, UpSampledDeconv, Extractor
